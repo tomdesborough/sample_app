f38c312b6cc5b58ea12faae50043f24146c5fa01
David Dinh <daviddinh93@gmail.com>
2016-09-26 08:30:59 +0000

    Basic Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
   
-  def hello
-    render html: "hello, world!"
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user.params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Willkommen to the Sample App"
+    redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
 end
+
